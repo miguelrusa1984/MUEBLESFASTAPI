8984f28df090b1cac601311de93f5996df67502a
miguelrusa1984 <miguelrusa1984@gmail.com>
2026-04-21 18:58:50 -0500

    apppy

--- muebles/app.py
+++ muebles/app.py
@@ -281,7 +281,6 @@ def eliminar_mueble_flask(muebleId):
 from conexión.config import app, db
 
 if __name__ == "__main__":
-
-    with app.app_context():
-        db.create_all()  
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    # Esto permite que Railway asigne el puerto automáticamente
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
